47100d141eb23a2cd56907549b2f61f8fb8115ea
LindvedKrvang <rklindved@gmail.com>
2017-08-17 10:20:19 +0200

    Able to select a menuOption.

--- VideoMenu/Program.cs
+++ VideoMenu/Program.cs
@@ -1,33 +1,20 @@
 ﻿using System;
+using VideoMenu.gui.controller;
 using VideoMenu.model;
 
 namespace VideoMenu
 {
     class Program
     {
-        private static MenuModel _menuModel;
+        private static MenuController _menuController;
 
         static void Main(string[] args)
         {
-            _menuModel = new MenuModel();
-            PrintWelcomeMessage();
-            DisplayMenu();
+            _menuController = new MenuController();
+            _menuController.PrintWelcomeMessage();
+            _menuController.MenuLoop();
         }
 
-        private static void PrintWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the Video Store!");
-        }
-
-        private static void DisplayMenu()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Please choose an option:");
-            var menuOptions = _menuModel.MenuOptions;
-            for (var i = 0; i < menuOptions.Count; i++)
-            {
-                Console.WriteLine($"{i} : {menuOptions[i]}");
-            }
-        }
+        
     }
 }
---
+++ VideoMenu/gui/controller/MenuController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenu.model;
+
+namespace VideoMenu.gui.controller
+{
+    class MenuController
+    {
+        private readonly MenuModel _menuModel;
+
+        private bool _programIsRunning;
+
+        public MenuController()
+        {
+            _menuModel = new MenuModel();
+            _programIsRunning = true;
+        }
+
+        /// <summary>
+        /// Prints a welcome message.
+        /// </summary>
+        public void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Video Store!");
+        }
+
+        /// <summary>
+        /// Keeps the program running until it's time terminate it.
+        /// </summary>
+        public void MenuLoop()
+        {
+            do
+            {
+                DisplayMenu();
+                var selectedMenuOption = PromptNumberForMenu();
+                HandleSelectedMenuOption(selectedMenuOption);
+            } while (_programIsRunning);
+        }
+
+        /// <summary>
+        /// Display the menu.
+        /// </summary>
+        private void DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Please choose an option:");
+            var menuOptions = _menuModel.MenuOptions;
+            for (var i = 0; i < menuOptions.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} : {menuOptions[i]}");
+            }
+        }
+
+        /// <summary>
+        /// Gets a number from the user. Validates it and returns it.
+        /// </summary>
+        /// <returns></returns>
+        private int PromptNumberForMenu()
+        {
+            int input;
+            if (!int.TryParse(Console.ReadLine(), out input) || input <= 0 || input > _menuModel.MenuOptions.Count)
+            {
+                Console.WriteLine("That's not a valid number for this menu. Please choose another:");
+                input = PromptNumberForMenu();
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// Takes the selectedOption and calls the appropiated method.
+        /// </summary>
+        /// <param name="selectedOption"></param>
+        private void HandleSelectedMenuOption(int selectedOption)
+        {
+            switch (selectedOption)
+            {
+                case 1:
+                {
+                    Console.WriteLine("Option 1 selected!");
+                    break;
+                }
+                case 2:
+                {
+                    Console.WriteLine("Option 2 selected!");
+                    break;
+                }
+                case 3:
+                {
+                    Console.WriteLine("Option 3 selected!");
+                    break;
+                }
+                case 4:
+                {
+                    Console.WriteLine("Option 4 selected!");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("No option selected!");
+                    break;
+                }
+            }
+        }
+    }
+}
