92d45309b2d8e980abbf059afc6736fc1ba0d0f7
LindvedKrvang <rklindved@gmail.com>
2017-08-23 10:16:04 +0200

    Added InMemoryDb

---
+++ VideoMenuDAL/Context/InMemoryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using VideoMenuEntities;
+
+namespace VideoMenuDAL.Context
+{
+    internal class InMemoryContext : DbContext
+    {
+        private static readonly DbContextOptions<InMemoryContext> Options = 
+            new DbContextOptionsBuilder<InMemoryContext>().UseInMemoryDatabase("TheDB").Options;
+
+        public InMemoryContext() : base(Options)
+        {
+            
+        }
+
+        public DbSet<Video> Videos { get; set; }
+    }
+}
---
+++ VideoMenuDAL/Repositories/VideoRepositoryInMemory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VideoMenuDAL.Context;
+using VideoMenuEntities;
+
+namespace VideoMenuDAL.Repositories
+{
+    internal class VideoRepositoryInMemory : IVideoRepository
+    {
+        private readonly InMemoryContext _context;
+
+        private static int _id = 1;
+
+        public VideoRepositoryInMemory(InMemoryContext context)
+        {
+            _context = context;
+        }
+
+        public List<Video> GetVidoes()
+        {
+            return _context.Videos.ToList();
+        }
+
+        public Video DeleteVideo(int idToRemove)
+        {
+            var videoToDelete = _context.Videos.FirstOrDefault(v => v.Id == idToRemove);
+            _context.Videos.Remove(videoToDelete);
+            _context.SaveChanges();
+            return videoToDelete;
+        }
+
+        public Video CreateVideo(string name)
+        {
+            var video = new Video(_id++, name, EGenre.Undefined);
+            _context.Videos.Add(video);
+            _context.SaveChanges();
+            return video;
+        }
+
+        public void UpdateAll(List<Video> videos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> SearchVideos(string searchQuery)
+        {
+            int.TryParse(searchQuery, out int id);
+            return _context.Videos.Where(v =>
+                    v.Name.ToLower().Contains(searchQuery.ToLower())
+                    || v.Id == id
+                    || v.Genre.ToString().ToLower().Contains(searchQuery.ToLower()))
+                .ToList();
+        }
+    }
+}
