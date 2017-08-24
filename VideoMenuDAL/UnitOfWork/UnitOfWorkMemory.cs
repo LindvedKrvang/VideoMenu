7900d0395c4de27238040bd0fa70752dcf41c076
LindvedKrvang <rklindved@gmail.com>
2017-08-24 09:58:31 +0200

    Created a UnitOfWork interface and a class implementing it

---
+++ VideoMenuDAL/UnitOfWork/UnitOfWorkMemory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuDAL.Context;
+using VideoMenuDAL.Repositories;
+
+namespace VideoMenuDAL.UnitOfWork
+{
+    internal class UnitOfWorkMemory : IUnitOfWork
+    {
+        public IVideoRepository VideoRepository { get; internal set; }
+
+        private readonly InMemoryContext _context;
+
+        public UnitOfWorkMemory()
+        {
+            _context = new InMemoryContext();
+            VideoRepository = new VideoRepositoryInMemory(_context);
+        }
+
+        /// <summary>
+        /// Call this method when the unitOfWork is ready to persist the data.
+        /// If this method isn't called, none of the data is saved.
+        /// </summary>
+        /// <returns></returns>
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Call this method to dispose of the connection to the repository.
+        /// </summary>
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
