5e09481427de64346a25b5acc371b759d4357636
LindvedKrvang <rklindved@gmail.com>
2017-08-25 09:28:29 +0200

    Added some unitTest

--- VideoMenuBLLTest/VideoServiceTests.cs
+++ VideoMenuBLLTest/VideoServiceTests.cs
@@ -25,9 +25,26 @@ namespace VideoMenuBLLTest
         };
 
         [Fact]
-        public void Test1()
+        public void GetVideoTest()
         {
+            var video = _videoService.GetVideo(1);
 
+            Assert.NotNull(video);
+        }
+
+        [Fact]
+        private void GetAllVideosTest()
+        {
+            var result = _videoService.GetVideos().Count;
+            var expectedResult = 4;
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        private void CreateVideoTest()
+        {
+            
         }
     }
 }
