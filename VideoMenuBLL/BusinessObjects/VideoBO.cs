c1770c91bb9aeac894ad3ee0e4910719757e749c
LindvedKrvang <rklindved@gmail.com>
2017-08-30 16:15:09 +0200

    Added the entitites in the other layers. Did not remove the layer yet

---
+++ VideoMenuBLL/BusinessObjects/VideoBO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VideoMenuBLL.BusinessObjects
+{
+    public class VideoBO : IComparable<VideoBO>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EGenreBO Genre { get; set; }
+
+        public VideoBO()
+        {
+            
+        }
+
+        public int CompareTo(VideoBO other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            var idComparison = Id.CompareTo(other.Id);
+            if (idComparison != 0) return idComparison;
+            var nameComparison = string.Compare(Name, other.Name, StringComparison.Ordinal);
+            if (nameComparison != 0) return nameComparison;
+            return Genre.CompareTo(other.Genre);
+        }
+    }
+}
---
+++ VideoMenuDAL/Entities/EGenre.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoMenuDAL.Entities
+{
+    public enum EGenre
+    {
+        Western,
+        Comedy,
+        Action,
+        Romantic,
+        Horror,
+        Undefined
+    } 
+}
